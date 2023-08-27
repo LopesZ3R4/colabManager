e058fa0532c5f1b00917858914bb2bbdb3580b06
LopesZ3R4 <jl.moreno7@hotmail.com>
2023-08-26 23:08:39 -0400

    Migração de Container, Reescrevendo os arquivos

--- DBinit.sql
+++ DBinit.sql
@@ -1,3 +1,9 @@
+-- Cria o schema "CM"
+CREATE SCHEMA IF NOT EXISTS CM;
+
+-- Usa o schema "CM"
+SET search_path TO CM;
+
 CREATE TABLE employee (
                           id SERIAL PRIMARY KEY,
                           name VARCHAR(255),
