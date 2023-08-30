c13655b0d2dc11f1be368eca18347733c1e68052
LopesZ3R4 <jl.moreno7@hotmail.com>
2023-08-29 20:07:53 -0400

    Adicionado Funcionalidades de Exclusão e Edição de funcionarios

--- src/main/java/com/colabmanager/controller/EmployeeController.java
+++ src/main/java/com/colabmanager/controller/EmployeeController.java
@@ -3,20 +3,16 @@ package com.colabmanager.controller;
 
 import com.colabmanager.model.Employee;
 import com.colabmanager.repository.EmployeeRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE})
 @RequestMapping("/employees")
 public class EmployeeController {
 
@@ -26,21 +22,104 @@ public class EmployeeController {
     public EmployeeController(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
 
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
-        System.out.println("Received Employee object: " + employee.toString()); // Imprime o objeto no console
-        return employeeRepository.save(employee);
+        // Log do IP e detalhes da requisição
+        System.out.println("Request from IP: " + request.getRemoteAddr());
+        System.out.println("Received Employee object: " + employee.toString());
+
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        // Log dos detalhes da resposta
+        System.out.println("Response status: " + response.getStatus());
+        System.out.println("Response body: " + savedEmployee.toString());
+
+        return savedEmployee;
     }
 
     @GetMapping
     public List<Employee> getEmployeeList() {
-        return employeeRepository.findAll();
+        // Log do IP e detalhes da requisição
+        System.out.println("Request from IP: " + request.getRemoteAddr());
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        // Log dos detalhes da resposta
+        System.out.println("Response status: " + response.getStatus());
+        System.out.println("Response body: " + employees.toString());
+
+        return employees;
     }
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable Long id) {
+        // Log do IP e detalhes da requisição
+        System.out.println("Request from IP: " + request.getRemoteAddr());
+
         Optional<Employee> employeeOptional = employeeRepository.findById(id);
-        return employeeOptional.orElse(null); // Retorna null se não for encontrado
+        Employee employee = employeeOptional.orElse(null);
+
+        // Log dos detalhes da resposta
+        System.out.println("Response status: " + response.getStatus());
+        System.out.println("Response body: " + (employee != null ? employee.toString() : "null"));
+
+        return employee;
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee) {
+        // Log do IP e detalhes da requisição
+        System.out.println("Request from IP: " + request.getRemoteAddr());
+        System.out.println("Updating employee with ID: " + id);
+        System.out.println("Request body: " + updatedEmployee.toString());
+
+        Optional<Employee> existingEmployeeOptional = employeeRepository.findById(id);
+        if (existingEmployeeOptional.isPresent()) {
+            existingEmployeeOptional.get();
+            Employee existingEmployee;
+            existingEmployee = employeeRepository.save(updatedEmployee);
+
+            // Log dos detalhes da resposta
+            System.out.println("Response status: " + response.getStatus());
+            System.out.println("Response body: " + existingEmployee.toString());
+
+            return ResponseEntity.ok(existingEmployee);
+        } else {
+            // Log dos detalhes da resposta
+            System.out.println("Response status: " + response.getStatus());
+            System.out.println("Response body: Employee with ID " + id + " not found.");
+
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployeeById(@PathVariable Long id) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+        // Log do IP e detalhes da requisição
+        System.out.println("Request from IP: " + request.getRemoteAddr());
+        System.out.println("Deleting employee with ID: " + id);
+
+        if (employeeOptional.isPresent()) {
+            Employee employee = employeeOptional.get();
+            employeeRepository.delete(employee);
+
+            // Log da ação
+            System.out.println("Employee with ID " + id + " deleted successfully.");
+            // Log dos detalhes da resposta
+            System.out.println("Response status: " + ResponseEntity.noContent().build());
+
+            return ResponseEntity.noContent().build(); // Retorna status 204 (No Content)
+        } else {
+            // Log da ação
+            System.out.println("Employee with ID " + id + " not found.");
+
+            // Log dos detalhes da resposta
+            System.out.println("Response status: " + ResponseEntity.notFound().build());
+            return ResponseEntity.notFound().build();
+        }
     }
 }
-
